7e752627b1dc48edb9aebf113e9e38d80b0a5474
volkan <16579273+mevolkan@users.noreply.github.com>
2023-12-15 13:24:34 +0300

    added navigation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
 				</div>
 			</div>
 		</div>
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
@@ -106,7 +106,28 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav> --}}
+
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+            <div class="container">
+                <a class="navbar-brand" href="index.html">Vacation<span>Rental</span></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fa fa-bars"></span> Menu
+              </button>
+              <div class="collapse navbar-collapse" id="ftco-nav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active"><a href="index.html" class="nav-link">Home</a></li>
+                    <li class="nav-item"><a href="about.html" class="nav-link">About</a></li>
+                    <li class="nav-item"><a href="services.html" class="nav-link">Services</a></li>
+                    <li class="nav-item"><a href="rooms.html" class="nav-link">Apartment Room</a></li>
+                  <li class="nav-item"><a href="contact.html" class="nav-link">Contact</a></li>
+                  <li class="nav-item"><a href="login.html" class="nav-link">Login</a></li>
+                  <li class="nav-item"><a href="register.html" class="nav-link">Register</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        <!-- END nav -->
 
         <main class="py-4">
             @yield('content')
