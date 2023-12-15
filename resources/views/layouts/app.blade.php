7a4b67563eccdb2e2eb850396b1b12c7b379bf59
volkan <16579273+mevolkan@users.noreply.github.com>
2023-12-15 13:17:31 +0300

    header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,23 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+    <link rel="stylesheet" href="{{asset ('assets/css/animate.css')}}">
+    
+    <link rel="stylesheet" href="{{asset ('assets/css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/css/owl.theme.default.min.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/css/magnific-popup.css')}}">
+
+    <link rel="stylesheet" href="{{asset ('assets/css/bootstrap-datepicker.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/css/jquery.timepicker.css')}}">
+
+    <link rel="stylesheet" href="{{asset ('assets/css/flaticon.css')}}">
+    <link rel="stylesheet" href="{{asset ('assets/css/style.css')}}">
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
