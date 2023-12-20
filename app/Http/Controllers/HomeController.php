fe39b82c7c3ead0b159ebd779b8da52a57112fdd
volkan <16579273+mevolkan@users.noreply.github.com>
2023-12-20 13:41:07 +0300

    added hotel to home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Hotel\hotel
 class HomeController extends Controller
 {
     /**
@@ -23,6 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $hotel = Hotel::select() ->orderBy('id','desc')->take(3)->get();
+        return view('home', compact('hotels'));
     }
 }
