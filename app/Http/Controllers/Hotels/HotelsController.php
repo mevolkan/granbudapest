5383f4a5d85808f6dafc85cedbbab682e9552a95
volkan <16579273+mevolkan@users.noreply.github.com>
2023-12-21 10:07:13 +0300

    booking scaffolding

--- app/Http/Controllers/Hotels/HotelsController.php
+++ app/Http/Controllers/Hotels/HotelsController.php
@@ -7,14 +7,29 @@ use App\Models\Apartment\Apartment;
 use Illuminate\Http\Request;
 
 class HotelsController extends Controller
-{
-    public function rooms($id){
+    {
+    public function rooms($id)
+        {
         $getRooms = Apartment::select()->orderBy('id', 'desc')->take(6)->where('hotel_id', $id)->get();
         return view('hotels.rooms', compact('getRooms'));
-    }
+        }
 
-    public function roomDetails($id){
+    public function roomDetails($id)
+        {
         $getRoom = Apartment::find($id);
         return view('hotels.roomdetails', compact('getRoom'));
+        }
+    public function roomBooking(Request $request, $id)
+        {
+        if ($date("Y/m/d") < $request->check_in and $date("Y/m/d") < $request->check_out) {
+            ///Continue with logic
+            if ($request->check_in < $request->check_out) {
+                ///Continue with logic
+                } else {
+                echo "checkout date is invalid";
+                }
+            } else {
+            echo "invalid check in or check out date";
+            }
+        }
     }
-}
