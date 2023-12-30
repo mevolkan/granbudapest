373e330f7accacdd460e54f8fd12b071e3838a49
volkan <16579273+mevolkan@users.noreply.github.com>
2023-12-30 18:28:56 +0300

    cleanup on url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
                 </button>
                 <div class="collapse navbar-collapse" id="ftco-nav">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+                        <li class="nav-item"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
                         <li class="nav-item"><a href="{{ route('about') }}" class="nav-link">About</a></li>
                         <li class="nav-item"><a href="{{ route('services') }}" class="nav-link">Services</a></li>
                         <li class="nav-item"><a href="{{ route('contact') }}" class="nav-link">Contact</a></li>
